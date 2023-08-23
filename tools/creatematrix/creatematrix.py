fa610cf084a078ded5eca25eebee871c7e6276d1
Tom Camp <tom.camp@civicactions.com>
2023-08-23 12:12:45 -0400

    Adding creatematrix.

---
+++ tools/creatematrix/creatematrix.py
@@ -0,0 +1,91 @@
+# Copyright 2019-2020 CivicActions, Inc. See the README file at the top-level
+# directory of this distribution and at https://github.com/CivicActions/compliancetools#copyright.
+
+import csv
+from collections import OrderedDict
+from pathlib import Path
+from typing import Optional
+
+import yaml
+
+from tools.lib import ssptoolkit
+
+
+def set_status(existing_status: str = "", component_status: str = "") -> Optional[str]:
+    new_status = None
+    if not existing_status and component_status:
+        new_status = component_status
+    elif component_status:
+        if component_status.lower() == "planned":
+            new_status = component_status
+        elif (
+            component_status.lower() == "partial"
+            and existing_status.lower() != "planned"
+        ):
+            new_status = component_status
+        elif component_status.lower() == "complete" and existing_status.lower() not in [
+            "planned",
+            "partial",
+        ]:
+            new_status = component_status
+        else:
+            new_status = component_status
+    return new_status
+
+
+def sort_data(components: dict, header: list):
+    try:
+        with open("keys/status.yaml", "r") as fp:
+            statuses = yaml.load(fp, Loader=yaml.SafeLoader)
+    except FileNotFoundError as error:
+        raise error
+
+    rows: list = []
+    for cid, control in components.items():
+        row: dict = OrderedDict.fromkeys(header)
+        row["Control"] = cid
+        row["Status"] = None
+        for component in control:
+            for key, satisfies in component.items():
+                status = statuses.get(satisfies.get("control_key"))
+                if not status:
+                    existing_status = row["Status"]
+                    component_status = satisfies.get("implementation_status", None)
+                    status = set_status(
+                        component_status=component_status,
+                        existing_status=existing_status,
+                    )
+                row["Status"] = status
+
+                row[key] = satisfies.get("security_control_type", None)
+        rows.append(row)
+
+    write_file(rows=rows)
+
+
+def write_file(rows: list):
+    output_to = Path("docs")
+    if not output_to.is_dir():
+        output_to.mkdir()
+
+    matrix = output_to.joinpath("responsibility_matrix.csv")
+    print(f"Writing file to {matrix}")
+    header = list(rows[0])
+    with open(matrix, "w+") as fp:
+        writer = csv.DictWriter(fp, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(rows)
+    print("Write complete")
+
+
+def main():
+    project = ssptoolkit.load_project_data()
+    components = project.get_components()
+    header: list = ["Control", "Status"] + [Path(c).name for c in components]
+    component_data = ssptoolkit.load_controls_by_id(component_list=components)
+
+    sort_data(components=component_data, header=header)
+
+
+if __name__ == "__main__":
+    main()
--- tools/makefamilies/makefamilies.py
+++ tools/makefamilies/makefamilies.py
@@ -1,66 +1,13 @@
 # Copyright 2019-2020 CivicActions, Inc. See the README file at the top-level
 # directory of this distribution and at https://github.com/CivicActions/compliancetools#copyright.
 
-import re
 from pathlib import Path
 
 import click
 import yaml
 
-from tools.lib.opencontrol import OpenControl
+from tools.lib import ssptoolkit
 from tools.makefamilies.family import Control, Family, Part
-
-
-def sortable_control_id(control_id: str) -> str:
-    return re.sub(r"(\d+)", lambda m: m.group(1).zfill(2), control_id)
-
-
-def get_standards() -> tuple:
-    try:
-        with open("opencontrol.yaml", "r") as oc:
-            project = yaml.load(oc, Loader=yaml.SafeLoader)
-    except FileNotFoundError as error:
-        print(f"File not found: {error}")
-
-    standards: list = []
-    for standard in project.get("standards"):
-        with open(standard, "r") as s:
-            standards.append(yaml.load(s, Loader=yaml.SafeLoader))
-
-    title = project.get("metadata").get("description")
-    return title, standards
-
-
-def get_standards_control_data(control: str, standards: list) -> dict:
-    also_try = control.replace("(", " (")
-    for s in standards:
-        if control_data := s.get(control):
-            return control_data
-        elif control_data := s.get(also_try):
-            return control_data
-    raise KeyError(f"Control {control} not found.")
-
-
-def get_standards_family_name(family: str, standards: list) -> str:
-    for s in standards:
-        if control_data := s.get(family, None):
-            return control_data.get("name")
-    raise KeyError(f"Control {family} not found.")
-
-
-def get_family_files(components: list) -> dict:
-    family_files: dict = {}
-    for c in components:
-        with open(Path(c).joinpath("component.yaml"), "r") as cfp:
-            component = yaml.load(cfp, Loader=yaml.SafeLoader)
-        for family in component.get("satisfies"):
-            component_file = Path().joinpath(c, family)
-            family_name = component_file.stem
-            if family_name not in family_files:
-                family_files[family_name] = []
-            family_files[family_name].append(component_file.as_posix())
-
-    return family_files
 
 
 def get_control_parts(parts: list, control, parent: str) -> Control:
@@ -86,8 +33,8 @@ def get_controls(family_files: list, family: Family, standards: list) -> Family:
         parent = component_file.parents[0].name
         for s in comp_yaml.get("satisfies"):
             control_id = s.get("control_key")
-            sort_id = sortable_control_id(control_id=control_id)
-            standard_data = get_standards_control_data(control_id, standards)
+            sort_id = ssptoolkit.sortable_control_id(control_id=control_id)
+            standard_data = ssptoolkit.get_standards_control_data(control_id, standards)
             status = s.get("implementation_status", "incomplete")
             if sort_id in family.controls:
                 control = family.controls.get(sort_id)
@@ -133,11 +80,11 @@ def create_toc(out_path: str, controls: dict):
 
 
 def create_family(families: dict, out_path: str):
-    title, standards = get_standards()
+    title, standards = ssptoolkit.get_standards()
     toc: dict = {}
     for key, family_files in families.items():
         fid = key[:2]
-        family_name = get_standards_family_name(fid, standards)
+        family_name = ssptoolkit.get_standards_family_name(fid, standards)
         family = Family(
             title=title,
             family_id=fid,
@@ -174,12 +121,8 @@ def main(output_dir):
         print(f"Creating output directory {controls_dir.resolve(strict=False)}")
         controls_dir.mkdir(exist_ok=False)
 
-    oc_yaml = base.joinpath("opencontrol.yaml")
-    if not oc_yaml.is_file():
-        raise FileNotFoundError
-
-    project = OpenControl.load(path=oc_yaml.as_posix())
-    family_files = get_family_files(project.get_components())
+    project = ssptoolkit.load_project_data()
+    family_files = ssptoolkit.get_component_files(project.get_components())
     create_family(family_files, out_path=output_dir)
     print("Process complete.")
 
