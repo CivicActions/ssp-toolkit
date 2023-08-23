7ee491a771e7179ff24241921ff4c0b960c79550
Tom Camp <tom.camp@civicactions.com>
2023-08-23 12:56:49 -0400

    Add poetry (#14)
    * Adding pyproject and initial tools.
    
    * Updating SC controls.
    
    * Updating gitignore.
    
    * Typo and URL fixes.

---
+++ tools/createfiles/createfiles.py
@@ -0,0 +1,115 @@
+# Copyright 2019-2020 CivicActions, Inc. See the README file at the top-level
+# directory of this distribution and at https://github.com/CivicActions/compliancetools#copyright.
+#
+# Given a YAML file and path to directory of template files, this tool
+# generates markdown files, replicating the directory structure in the template
+# directory. It uses the https://github.com/CivicActions/secrender tool for
+# variable replacement.
+
+import mmap
+from itertools import dropwhile, zip_longest
+from pathlib import Path
+
+import click
+import md_toc
+from yaml import FullLoader, load  # type: ignore
+from yamlinclude import YamlIncludeConstructor
+
+from tools.lib import secrender
+
+
+@click.command()
+@click.option(
+    "--in",
+    "-i",
+    "config_file",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False, readable=True),
+    help="Replacement data values (YAML)",
+)
+@click.option(
+    "--templates",
+    "-t",
+    "templates",
+    required=True,
+    type=click.Path(exists=True, dir_okay=True, file_okay=False),
+    help="Template directory",
+)
+@click.option(
+    "--out",
+    "-o",
+    "output_dir",
+    type=click.Path(exists=False, dir_okay=True, readable=True),
+    default=".",
+    help="Output directory (default: current directory)",
+)
+def main(config_file: str, templates: str, output_dir: str):
+    template_args = load_template_args(config_file)
+    output_to = Path(output_dir)
+    template_dir = Path(templates)
+    if not output_to.is_dir():
+        output_to.mkdir(parents=True, exist_ok=True)
+
+    template_path = Path(template_dir).rglob("*")
+    template_files = [x for x in template_path if x.is_file()]
+
+    for template in template_files:
+        new_file = Path(
+            rewrite(
+                template_file=template,
+                template_dir=template_dir,
+                output_dir=output_to,
+            )
+        )
+        if new_file.suffix == ".j2":
+            new_file = new_file.with_name(new_file.stem)
+
+        if not new_file.parent.is_dir():
+            new_file.parent.mkdir(parents=True, exist_ok=True)
+        print(f"Creating file: {new_file} from {template}")
+
+        secrender.secrender(
+            template_path=template.as_posix(),
+            template_args=template_args,
+            output_path=new_file.as_posix(),
+        )
+
+        find_toc_tag(file=str(new_file))
+
+
+def load_template_args(config_file: str) -> dict:
+    YamlIncludeConstructor.add_to_loader_class(loader_class=FullLoader)
+    with open(config_file, "r", newline="") as stream:
+        yaml = load(stream, Loader=FullLoader)
+    return secrender.get_template_args(yaml=yaml, set_={}, root=None)
+
+
+def rewrite(template_file: Path, template_dir: Path, output_dir: Path) -> str:
+    sub_path = [
+        p[0]
+        for p in dropwhile(
+            lambda f: f[0] == f[1], zip_longest(template_file.parts, template_dir.parts)
+        )
+    ]
+    return str(output_dir / Path(*sub_path))
+
+
+def find_toc_tag(file: str):
+    with open(file, "rb", 0) as f, mmap.mmap(
+        f.fileno(), 0, access=mmap.ACCESS_READ
+    ) as s:
+        if s.find(b"<!--TOC-->") != -1:
+            write_toc(file)
+
+
+def write_toc(file: str):
+    toc = md_toc.build_toc(filename=file, keep_header_levels=3, skip_lines=5)
+    md_toc.write_string_on_file_between_markers(
+        filename=file,
+        string=toc,
+        marker="<!--TOC-->",
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/makefamilies/family.py
@@ -0,0 +1,81 @@
+from collections import OrderedDict
+from pathlib import Path
+from typing import Dict, List
+
+from pydantic import BaseModel
+
+
+def sort_by_keys(to_sort: dict) -> OrderedDict:
+    return OrderedDict(sorted(to_sort.items()))
+
+
+class Part(BaseModel):
+    key: str
+    party: str
+    narrative: str
+
+    def __str__(self):
+        return f"#### {self.party}\n\n{self.narrative}\n"
+
+
+class Control(BaseModel):
+    control_id: str
+    control_name: str
+    description: str
+    control_type: str
+    status: str
+    parts: Dict[str, List[Part]]
+
+    def header(self) -> str:
+        return f"### {self.control_id}: {self.control_name.title()}\n\n"
+
+    def control_description(self) -> str:
+        return f"```text\n{self.description}\n```\n"
+
+    def get_status(self) -> str:
+        return f"**Status:** {self.status}\n"
+
+    def add_part(self, pid: str, part: Part):
+        if pid not in self.parts:
+            self.parts[pid] = []
+        self.parts[pid].append(part)
+
+    def get_parts(self):
+        parts: str = ""
+        self.parts = sort_by_keys(self.parts)
+        for key, part in self.parts.items():
+            for p in part:
+                part_key = "" if key == "_default" else key
+                parts = parts + f"{part_key}\n{str(p)}\n"
+        return parts
+
+    def __str__(self):
+        return f"{self.header()}{self.control_description()}{self.get_status()}{self.get_parts()}"
+
+
+class Family(BaseModel):
+    title: str
+    family_id: str
+    family_name: str
+    controls: Dict[str, Control]
+
+    def header(self):
+        return f"# {self.title}\n\n## {self.family_id}: {self.family_name}\n\n"
+
+    def add_control(self, cid: str, control: Control):
+        self.controls[cid] = control
+
+    def get_controls(self) -> str:
+        text: str = ""
+        for key, control in self.controls.items():
+            text = text + str(control)
+        return text
+
+    def print_family_file(self, out_path: str):
+        self.controls = sort_by_keys(self.controls)
+        content = self.header() + self.get_controls()
+        output = Path(out_path).joinpath(self.family_id).with_suffix(".md")
+        with open(output, "w+") as ofp:
+            ofp.write(content)
+
+        print(f"writing file {output.as_posix()}")
