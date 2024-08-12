1196f13d12a38bc9715114e2a1613c2bba9c23a9
Tom Camp <tom.camp@civicactions.com>
2024-08-12 10:03:31 -0400

    Adding click commands.

--- tools/helpers/config.py
+++ tools/helpers/config.py
@@ -8,6 +8,7 @@ class Config:
     config: dict
     configuration: Path = Path("configuration.yaml")
     keys: Path = Path("keys")
+    config_files: list[()] = []
     default_keys: dict = {
         "responsibility.yaml": "status",
         "info_system.yaml": "information_system",
@@ -28,33 +29,51 @@ class Config:
 
     def load_keys(self):
         for filename in self.keys.glob("*.yaml"):
-            print(f"Loading {filename.name}")
             key = self.default_keys.get(filename.name, filename.stem)
+            self.config_files.append((filename.name, key))
             with open(filename, "r") as fp:
                 self.config[key] = yaml.load(fp, Loader=yaml.FullLoader)
 
-    def check_config(self, key: str = "", verbose: bool = False):
+    def check_config_values(self, file: str, key: str = ""):
         if key:
-            print(yaml.dump(self.config.get(key, {})))
+            click.echo(self.config.get(file, {}).get(key, ""))
         else:
-            print(yaml.dump(self.config, default_flow_style=False, indent=4, width=80))
+            click.echo(yaml.dump(self.config.get(file, {}), indent=4, width=80))
 
 
-@click.command()
+@click.group()
+@click.pass_context
+def check_config(ctx):
+    ctx.obj = Config()
+
+
+@check_config.command()
+@click.option(
+    "--file",
+    "-f",
+    required=True,
+)
 @click.option(
     "--key",
     "-k",
+    required=False,
+    help="The name of the configuration key whose value should be shown.",
 )
-@click.option(
-    "--verbose",
-    "-v",
-    is_flag=True,
-)
-def main(key: str = "", verbose: bool = False):
-    config = Config()
-    if key and not verbose:
-        print(config.check_config())
+@click.pass_context
+def get_value(ctx, file: str, key: str = ""):
+    config = ctx.obj
+    config.check_config_values(file, key)
+
+
+@check_config.command()
+@click.pass_context
+def list_files(ctx):
+    config = ctx.obj
+    click.echo("Key files and configuration keys:")
+    click.echo("---------------------------------")
+    for files in config.config_files:
+        click.echo(f"{files[0]} using alias {files[1]}")
 
 
 if __name__ == "__main__":
-    main()
+    check_config()
